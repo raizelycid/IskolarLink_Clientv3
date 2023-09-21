8129c821a36d40e4d058b0147bcd35f5e6dd6cc3
Renee Sarmiento <72420468+CptRainy@users.noreply.github.com>
2023-09-20 21:29:51 -0700

    Sec2

---
+++ client/src/components/Section2.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import './general.css';
+
+const Section2 = () => {
+    return (
+        <div className="vh-100 bg-yellow Inter text-black">
+            <Container>
+                <Row>
+                    <h1>PUP Commission on Student Organizations and Accreditation (COSOA)</h1>
+                    <p>An independent institution of the PUP Student Council which have the mandate to accredit and reaccredit.</p>
+                </Row>
+                <Row>
+                    <Col>1 of 2
+                    </Col>
+                    <Col> 2 of 2
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Section2;
