8462fc75a8f972249f7752e0f6297565622ca5c3
Renee Sarmiento <72420468+CptRainy@users.noreply.github.com>
2023-10-05 07:16:36 -0700

    Announcement Card

--- client/src/components/Section3.js
+++ client/src/components/Section3.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './general.css';
-import { Row } from "react-bootstrap";
+import './general.css'; // Your existing CSS
+import { Row, Col, Container } from "react-bootstrap";
+import AnnouncementCard from './AnnouncementCard.js'
 
 const Section3 = () => {
   return (
@@ -9,6 +10,43 @@ const Section3 = () => {
         <h1 className="s3-h1 text-red Inter-b">Announcements</h1>
         <p className="s3-p text-gray2 Inter-normal">Discover the latest announcement that will shape the future of PUP COSOA and<br/>elevate your student experience!</p>
       </Row>
+      <Container fluid className="d-flex justify-content-center pb-5"> {/* Apply the custom class */}
+        <Row>
+          <Col className='d-flex justify-content-center'>
+            <AnnouncementCard
+              imageSrc="studentorg.png"
+              title="Important Announcement"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              userName="John Doe"
+              userType="Announcement"
+              date="Oct 5"
+              userImageSrc="s1-icon1.png"
+            />
+          </Col>
+          <Col className='d-flex justify-content-center'>
+            <AnnouncementCard
+              imageSrc="studentorg.png"
+              title="Important Announcement"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              userName="John Doe"
+              userType="Announcement"
+              date="Oct 5"
+              userImageSrc="s1-icon1.png"
+            />
+          </Col>
+          <Col className='d-flex justify-content-center'>
+            <AnnouncementCard
+              imageSrc="studentorg.png"
+              title="Important Announcement"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              userName="John Doe"
+              userType="Announcement"
+              date="Oct 5"
+              userImageSrc="s1-icon1.png"
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
