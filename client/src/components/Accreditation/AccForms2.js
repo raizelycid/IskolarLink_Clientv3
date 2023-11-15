7774a3fdea80b08306dd5147ae471a95afc3167b
Lukemark02082 <72384016+Lukemark02082@users.noreply.github.com>
2023-11-15 10:25:39 +0800

    Accreditation, Org_Settings FrontEnd

--- client/src/components/Accreditation/AccForms2.js
+++ client/src/components/Accreditation/AccForms2.js
@@ -1,11 +1,17 @@
 import {React, useState} from 'react'
-import { Form, ToggleButton, Row, Col, Button } from 'react-bootstrap'
+import './AccFinish.css';
+import { Form, Image, ToggleButton, Row, Col, Button } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container';
 
 const AccForms2 = ({formData, setFormData,show,path,path2}) => {
 
     const [checked, setChecked] = useState(false);
   return (
+    <>
     <form style={{display:show}}>
+        <Container>
+        <Image src="/3rd.png" alt="IskolarLink Logo"  fluid style={{ marginBottom: '40px' }}/>    
+        <Row className="p-4">
         <Form.Group as={Row} controlId='AF001' className="mb-3">
             <Form.Label>Upload Signed Tracker Form <span className='accreditation-download' onClick={() => window.open(`http://localhost:3001/${path}`,'_blank','noopener')}>(Download Generated File Here)</span></Form.Label>
             <Form.Control type="file" placeholder='e.g. AF001.pdf' onChange={(e) => setFormData({...formData, AF001: e.target.files[0]})}/>
@@ -14,16 +20,26 @@ const AccForms2 = ({formData, setFormData,show,path,path2}) => {
             <Form.Label>Upload Signed Waver Form <span className='accreditation-download' onClick={() => window.open(`http://localhost:3001/${path2}`,'_blank','noopener')}>(Download Generated File Here)</span></Form.Label>
             <Form.Control type="file" placeholder='e.g. AD009.pdf' onChange={(e) => setFormData({...formData, AD009: e.target.files[0]})}/>
         </Form.Group>
-        <ToggleButton
-            id="privacy-policy-toggle"
+        <Form.Group>
+        <Form.Check
             type="checkbox"
+            id="privacy-policy-toggle"
             variant="outline-primary"
-            checked={checked}
+            className="custom-checkbox"
             value="1"
+            label={
+            <>
+                I agree to the <span className='accreditation-download'>Privacy Policy</span>
+            </>
+            }
+            checked={checked}
             onChange={(e) => setChecked(e.currentTarget.checked)}
         />
-        <Form.Label>I agree to the <span className='accreditation-download'>Privacy Policy</span></Form.Label>
+        </Form.Group>
+        </Row>
+        </Container>
     </form>
+    </>
   )
 }
 
