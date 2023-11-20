c2b958be31b4ca4635b3b71e98a99b20af060089
Lukemark02082 <72384016+Lukemark02082@users.noreply.github.com>
2023-11-21 00:27:04 +0800

    edit reval form

--- client/src/components/Revalidation/RevForms1.js
+++ client/src/components/Revalidation/RevForms1.js
@@ -12,40 +12,45 @@ const RevForms1 = ({formData, setFormData, show}) => {
     <Image src="/2nd.png" alt="IskolarLink Logo"  fluid style={{ marginBottom: '40px' }}/>    
         <Row className="p-4">
         <Col>
-            <Form.Group as={Row} controlId='AD001' className="mb-3 form-group-spacing">
+            <Form.Group as={Row} controlId='RD001' className="mb-3 form-group-spacing">
                 <Form.Label>Upload Certificate of Recognition from Central/Local Student Council</Form.Label>
-                <Form.Control type="file" placeholder='e.g. AD001.pdf' onChange={(e) => setFormData({...formData, AD001: e.target.files[0]})}/>
+                <Form.Control type="file" placeholder='e.g. RD001.pdf' onChange={(e) => setFormData({...formData, RD001: e.target.files[0]})}/>
             </Form.Group>
-            <Form.Group as={Row} controlId='AD003' className="mb-3 form-group-spacing">
+            <Form.Group as={Row} controlId='RD003' className="mb-3 form-group-spacing">
                 <Form.Label>Upload Officers' Profile with Proof of Enrollment</Form.Label>
-                <Form.Control type="file" placeholder='e.g. AD003.pdf' onChange={(e) => setFormData({...formData, AD003: e.target.files[0]})}/>
+                <Form.Control type="file" placeholder='e.g. RD003.pdf' onChange={(e) => setFormData({...formData, RD003: e.target.files[0]})}/>
             </Form.Group>
-            <Form.Group as={Row} controlId='AD005' className="mb-3 form-group-spacing">
+            <Form.Group as={Row} controlId='RD005' className="mb-3 form-group-spacing">
                 <Form.Label>Upload Constitution and Bylaws</Form.Label>
-                <Form.Control type="file" placeholder='e.g. AD005.pdf' onChange={(e) => setFormData({...formData, AD005: e.target.files[0]})}/>
+                <Form.Control type="file" placeholder='e.g. RD005.pdf' onChange={(e) => setFormData({...formData, RD005: e.target.files[0]})}/>
             </Form.Group>
-            <Form.Group as={Row} controlId='AD007' className="mb-3 form-group-spacing">
+            <Form.Group as={Row} controlId='RD007' className="mb-3 form-group-spacing">
                 <Form.Label>Upload Advocacy Plan</Form.Label>
-                <Form.Control type="file" placeholder='e.g. AD007.pdf' onChange={(e) => setFormData({...formData, AD007: e.target.files[0]})}/>
+                <Form.Control type="file" placeholder='e.g. RD007.pdf' onChange={(e) => setFormData({...formData, RD007: e.target.files[0]})}/>
+            </Form.Group>
+            <Form.Group as={Row} controlId='RD010' className="mb-3 form-group-spacing">
+                <Form.Label>Upload Turnover of Assets and Funds</Form.Label>
+                <Form.Control type="file" placeholder='e.g. RD010.pdf' onChange={(e) => setFormData({...formData, RD010: e.target.files[0]})}/>
             </Form.Group>
         </Col>
         <Col>
-            <Form.Group as={Row} controlId='AD002' className="mb-3 form-group-spacing">
+            <Form.Group as={Row} controlId='RD002' className="mb-3 form-group-spacing">
                 <Form.Label>Upload Official List of Officers, Members, and Adviser(s)</Form.Label>
-                <Form.Control type="file" placeholder='e.g. AD002.pdf' onChange={(e) => setFormData({...formData, AD002: e.target.files[0]})}/>
+                <Form.Control type="file" placeholder='e.g. RD002.pdf' onChange={(e) => setFormData({...formData, RD002: e.target.files[0]})}/>
             </Form.Group>
-            <Form.Group as={Row} controlId='AD004' className="mb-3 form-group-spacing">
+            <Form.Group as={Row} controlId='RD004' className="mb-3 form-group-spacing">
                 <Form.Label>Upload Letter of Concurrence</Form.Label>
-                <Form.Control type="file" placeholder='e.g. AD004.pdf' onChange={(e) => setFormData({...formData, AD004: e.target.files[0]})}/>
+                <Form.Control type="file" placeholder='e.g. RD004.pdf' onChange={(e) => setFormData({...formData, RD004: e.target.files[0]})}/>
             </Form.Group>
-            <Form.Group as={Row} controlId='AD006' className="mb-3 form-group-spacing">
+            <Form.Group as={Row} controlId='RD006' className="mb-3 form-group-spacing">
                 <Form.Label>Upload General Plan of Action</Form.Label>
-                <Form.Control type="file" placeholder='e.g. AD006.pdf' onChange={(e) => setFormData({...formData, AD006: e.target.files[0]})}/>
+                <Form.Control type="file" placeholder='e.g. RD006.pdf' onChange={(e) => setFormData({...formData, RD006: e.target.files[0]})}/>
             </Form.Group>
-            <Form.Group as={Row} controlId='AD008' className="mb-3 form-group-spacing">
+            <Form.Group as={Row} controlId='RD008' className="mb-3 form-group-spacing">
                 <Form.Label>Upload Certificate of Clearance from PUP Student Council COA</Form.Label>
-                <Form.Control type="file" placeholder='e.g. AD008.pdf' onChange={(e) => setFormData({...formData, AD008: e.target.files[0]})}/>
+                <Form.Control type="file" placeholder='e.g. RD008.pdf' onChange={(e) => setFormData({...formData, RD008: e.target.files[0]})}/>
             </Form.Group>
+            
         </Col>
         </Row>
         </Container>
